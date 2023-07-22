120a15d1568862268ff54f43011a92ec76402ec7
IlyaPalatov <121305312+IlyaPalatov@users.noreply.github.com>
2023-07-22 09:29:38 +0300

    Finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,63 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import '../styles.css';
+
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [],
+      filter: '',
+    };
+  }
+
+  componentDidMount() {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      this.setState({ contacts: JSON.parse(storedContacts) });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = newContact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  setFilter = filterValue => {
+    this.setState({ filter: filterValue });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    return (
+      <div>
+        <h1 className="app-title">Phonebook</h1>
+        <ContactForm contacts={contacts} addContact={this.addContact} />
+
+        <h2 className="app-subtitle">Contacts</h2>
+        <Filter filter={filter} setFilter={this.setFilter} />
+        <ContactList
+          contacts={contacts}
+          filter={filter}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
